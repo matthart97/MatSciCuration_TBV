145a68ec45c6eed86a18dc23b044caf3901bf2aa
Matt <mattthart@gmail.com>
2024-08-13 10:13:46 -0400

    Added final pooling files for dyes

--- PreCuration/Dyes/DyePooling.py
+++ PreCuration/Dyes/DyePooling.py
@@ -8,8 +8,18 @@ PATH = os.getcwd()
 data_path = PATH +"/Dye_lake/"
 
 df1 = pd.read_csv(data_path+"deep4chem.csv") #changed chromophre to SMILES
+
+exclude = ['SMILES','Tag']
+df1.rename(columns=lambda x: x + ('SolventDependent') if x not in exclude else x, inplace=True)
+df1 = df1.drop(['Tag'], axis =1)
+
+
 df2 = pd.read_csv(data_path+"ocelot_data.csv")
+
+
 df3 = pd.read_csv(data_path+"WDVDataComplete_uncurated.csv")
+
+df3.replace({1.0: 'yes', 0.0: 'unknown', -1.0: 'no'}, inplace=True)
 
 df1['source'] = 'D4C' #deep4chem
 df2['source'] = 'OCT'
@@ -28,4 +38,4 @@ merged_df = df1.merge(df2, on='SMILES', how='outer')
 
 merged_df = merged_df.merge(df3,on='SMILES', how='outer')
 # Write the merged dataset to a CSV file
-merged_df.to_csv('DyeData_Uncurated.csv', index=False)
+merged_df.to_csv('DyeDataUncurated.csv', index=False)
